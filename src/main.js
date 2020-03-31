c0cc6ee1317b3c00641e8e04aa1e379090d205e7
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-31 17:39:27 +0200

    add rendering loop

--- src/main.js
+++ src/main.js
@@ -12,20 +12,31 @@ ctx.strokeStyle = '#fff'
 const shipSize = 20
 const asteroidSize = 20
 
-ctx.save()
-ctx.rotate(0)
-ctx.translate(canvas.width / 2, canvas.height / 2)
+function shipDraw () {
+  ctx.save()
+  ctx.rotate(0)
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.beginPath()
+  ctx.moveTo(0, -1.5 * shipSize / 2)
+  ctx.lineTo(shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
+  ctx.lineTo(-shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
+  ctx.closePath()
+  ctx.stroke()
+  ctx.restore()
+}
 
-ctx.beginPath()
-ctx.moveTo(0, -1.5 * shipSize / 2)
-ctx.lineTo(shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
-ctx.lineTo(-shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
-ctx.closePath()
-ctx.stroke()
-ctx.restore()
+function asteroidDraw () {
+  ctx.save()
+  ctx.rotate(0.1)
+  ctx.translate(50, 50)
+  ctx.strokeRect(-asteroidSize / 2, -asteroidSize / 2, asteroidSize, asteroidSize)
+  ctx.restore()
+}
 
-ctx.save()
-ctx.rotate(0.1)
-ctx.translate(50,50)
-ctx.strokeRect(-asteroidSize / 2, -asteroidSize / 2, asteroidSize, asteroidSize)
-ctx.restore()
+animate()
+
+function animate(){
+  window.requestAnimationFrame(animate)
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+  shipDraw()
+}
