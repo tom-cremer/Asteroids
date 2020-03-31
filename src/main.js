faa09ca947a719a84739206eec34a8382e95d5e1
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-31 17:00:13 +0200

    Add multiple context transformations

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ const ctx = canvas.getContext('2d')
 ctx.strokeStyle = '#fff'
 
 const shipSize = 20
+const asteroidSize = 20
 
+ctx.save()
 ctx.rotate(0)
 ctx.translate(canvas.width / 2, canvas.height / 2)
 
@@ -20,3 +22,10 @@ ctx.lineTo(shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
 ctx.lineTo(-shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
 ctx.closePath()
 ctx.stroke()
+ctx.restore()
+
+ctx.save()
+ctx.rotate(0.1)
+ctx.translate(50,50)
+ctx.strokeRect(-asteroidSize / 2, -asteroidSize / 2, asteroidSize, asteroidSize)
+ctx.restore()
