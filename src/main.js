217f6f6f6c994037debcdc89bb252fe0f7b70d23
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-31 18:08:03 +0200

    add minimalist motion of ship

--- src/main.js
+++ src/main.js
@@ -11,11 +11,21 @@ ctx.strokeStyle = '#fff'
 
 const shipSize = 20
 const asteroidSize = 20
+const shipSpeedX = 1
+let shipLocationX = canvas.width / 2
+
+function shipUpdate () {
+  shipLocationX += shipSpeedX
+  if (shipLocationX > canvas.width + shipSize) {
+    shipLocationX = -shipSize
+  }
+  shipDraw()
+}
 
 function shipDraw () {
   ctx.save()
   ctx.rotate(0)
-  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.translate(shipLocationX, canvas.height / 2)
   ctx.beginPath()
   ctx.moveTo(0, -1.5 * shipSize / 2)
   ctx.lineTo(shipSize / 2, 0.5 + (shipSize * 1.5 / 2))
@@ -35,8 +45,8 @@ function asteroidDraw () {
 
 animate()
 
-function animate(){
+function animate () {
   window.requestAnimationFrame(animate)
-  ctx.clearRect(0,0,canvas.width, canvas.height)
-  shipDraw()
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  shipUpdate()
 }
