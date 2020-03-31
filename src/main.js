dbc62e5afdc7b69173751c264456048de324db00
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-31 19:29:22 +0200

    refactoring - create object for mail

--- src/main.js
+++ src/main.js
@@ -1,17 +1,40 @@
 import ship from './ship'
 
-const main = document.getElementById('asteroids')
-document.body.removeChild(main)
+const main = {
+  mainElt: null,
+  canvasElt: null,
+  canvasEltDimensions: {
+    width: 640,
+    height: 480
+  },
+  ctx: null,
 
-const canvas = document.createElement('canvas')
-document.body.insertAdjacentElement('afterbegin', canvas)
-canvas.width = 640
-canvas.height = 480
+  init () {
+    this.mainElt = document.getElementById('asteroids')
+    document.body.removeChild(this.mainElt)
 
-const ctx = canvas.getContext('2d')
-ctx.strokeStyle = '#fff'
+    this.canvasElt = document.createElement('canvas')
+    document.body.insertAdjacentElement('afterbegin', this.canvasElt)
+    this.canvasElt.width = this.canvasEltDimensions.width
+    this.canvasElt.height = this.canvasEltDimensions.height
 
-ship.init(canvas, ctx)
+    this.ctx = this.canvasElt.getContext('2d')
+    this.ctx.strokeStyle = '#fff'
+
+    ship.init(this.canvasElt, this.ctx)
+    this.animate()
+  },
+
+  animate () {
+    window.requestAnimationFrame(()=>{
+      this.animate()
+    })
+    this.ctx.clearRect(0, 0, this.canvasElt.width, this.canvasElt.height)
+    ship.update()
+  }
+}
+
+main.init()
 
 const asteroidSize = 20
 
@@ -21,12 +44,4 @@ function asteroidDraw () {
   ctx.translate(50, 50)
   ctx.strokeRect(-asteroidSize / 2, -asteroidSize / 2, asteroidSize, asteroidSize)
   ctx.restore()
-}
-
-animate()
-
-function animate () {
-  window.requestAnimationFrame(animate)
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ship.update()
 }
