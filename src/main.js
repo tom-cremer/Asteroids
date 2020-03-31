0ae8f6f840e8473ba8a8593ad337956f05a977a4
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-31 13:05:42 +0200

    update coding styles

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const main = document.getElementById('asteroids')
 document.body.removeChild(main)
 
 const canvas = document.createElement('canvas')
-document.body.insertAdjacentElement("afterbegin", canvas)
+document.body.insertAdjacentElement('afterbegin', canvas)
 canvas.width = 640
 canvas.height = 480
-
