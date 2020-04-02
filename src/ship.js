ce45dd55d4e3cbecb3d1980e0642893898d2cd55
Dominique Vilain <dominique.vilain@hepl.be>
2020-04-02 13:55:48 +0200

    add heading of ship

--- src/ship.js
+++ src/ship.js
@@ -5,6 +5,7 @@ const ship = {
 
   size: 20,
   speed: null,
+  heading: Math.PI/2,
   location: null,
   acceleration: null,
   canvas: null,
@@ -37,8 +38,8 @@ const ship = {
 
   draw () {
     this.ctx.save()
-    this.ctx.rotate(0)
     this.ctx.translate(this.canvas.width / 2, this.location.y)
+    this.ctx.rotate(this.heading)
     this.ctx.beginPath()
     this.ctx.moveTo(0, -1.5 * this.size / 2)
     this.ctx.lineTo(this.size / 2, 0.5 + (this.size * 1.5 / 2))
