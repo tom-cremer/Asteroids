7c32ac456722d6e294bcf09fb0192a7662b252b4
Dominique Vilain <dominique.vilain@hepl.be>
2020-04-03 16:31:39 +0200

    add movement to Asteroids

--- src/Asteroid.js
+++ src/Asteroid.js
@@ -6,20 +6,25 @@ export default class Asteroid {
     this.canvas = canvas
     this.ctx = ctx
     this.size = 10
+    this.heading = Math.random() * Math.PI * 2
     this.location = new Vector(Math.random() * this.canvas.width, Math.random() * this.canvas.height)
+    this.speed = new Vector(0, 0)
+    this.acceleration = Vector.fromAngle(this.heading, 2 + Math.random() * 2)
+    this.speed.add(this.acceleration)
     const asCount = asteroidShapes.length
     const i = Math.floor(Math.random() * asCount)
     this.shape = asteroidShapes[i]
   }
 
   update () {
+    this.location.add(this.speed)
     this.draw()
   }
 
   draw () {
     this.ctx.save()
     this.ctx.translate(this.location.x, this.location.y)
-    //this.ctx.rotate(this.heading)
+    this.ctx.rotate(this.heading)
     this.ctx.beginPath()
     this.ctx.moveTo(this.shape[0] * this.size, this.shape[1] * this.size)
     let i = 2
@@ -29,7 +34,7 @@ export default class Asteroid {
       i++
     }
     this.ctx.closePath()
-    this.ctx.stroke()
+    this.ctx.fill()
     this.ctx.restore()
   }
 }
